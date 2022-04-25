fa9378df5cbb3b71702bed99ac09e925f58c428b
karen-yquevedo <kyamilequevedo@gmail.com>
2022-04-25 16:09:16 +0200

    add index html & js

--- index.js
+++ index.js
@@ -10,5 +10,32 @@ console.log(h);
 let y = 10;
 console.log(y);
 
-let z = 'h' + 'y';
+let z = {y, h};
 console.log (z);
+
+const character = {name: 'Jack Sparrow', age: 10};
+age = 25;
+console.log (age);
+
+let = {firstName: 'Jon', lastName: 'Snow', age: 24};
+console.log ('Soy', firstName,  lastName, 'tengo', age, 'y me gustan los lobos.');
+
+const toy1 = {name: 'Buss myYear', price: 19};
+const toy2 = {name: 'Rallo mcKing', price: 29};
+console.log = (toy1, toy2);
+
+let globalBasePrice = 10000;
+lobalBasePrice = 25000;
+
+console.log (globalBasePrice);
+
+const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
+const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
+
+finalPrice = (basePrice, globalBasePrice);
+console.log (finalPrice);
+
+finalPrice = (basePrice, finalPrice);
+console.log (finalPrice);
+
+
